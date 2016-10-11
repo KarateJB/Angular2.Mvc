2b457c6530201fa4d3069cfd45bbd639d90e155b
KarateJB <ppippenkimo@gamil.com>
2016-10-11 10:23:13 +0800

    New sample project

---
+++ Angular2.Mvc/src/Angular2.Mvc.Website/gulpfile.js
@@ -0,0 +1,41 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+"use strict";
+var gulp = require('gulp');
+var root_path = {
+    webroot: "./wwwroot/"
+}
+
+//library source
+root_path.nmSrc = "./node_modules/";
+//library destination
+root_path.package_lib = root_path.webroot + "lib-npm/"
+
+//rxjs
+gulp.task("copy-rxjs", function () {
+    return gulp.src(root_path.nmSrc + '/rxjs/bundles/*.*', {
+        base: root_path.nmSrc + '/rxjs/bundles/'
+    }).pipe(gulp.dest(root_path.package_lib + '/rxjs/'));
+});
+//angular2
+gulp.task('copy-angular2', function () {
+    return gulp.src(root_path.nmSrc + "/angular2/bundles/**/*.js", {
+        base: root_path.nmSrc + '/angular2/bundles/'
+    }).pipe(gulp.dest(root_path.package_lib + '/angular2/'));
+});
+//systemjs
+gulp.task('copy-systemjs', function () {
+    return gulp.src(root_path.nmSrc + "/systemjs/dist/**/*.*", {
+        base: root_path.nmSrc + '/systemjs/dist/'
+    }).pipe(gulp.dest(root_path.package_lib + '/systemjs'));
+});
+//ES6
+gulp.task('copy-es6-shim', function () {
+    return gulp.src(root_path.nmSrc + "/es6-shim/es6-sh*", {
+        base: root_path.nmSrc + '/es6-shim/'
+    }).pipe(gulp.dest(root_path.package_lib + '/es6-shim'));
+});
+
+gulp.task("copy-all", ["copy-rxjs", "copy-angular2", "copy-systemjs", "copy-es6-shim"])
---
+++ Angular2.Mvc/src/Angular2.Mvc.Website/wwwroot/app/app.component.ts
@@ -0,0 +1,7 @@
+﻿import { Component, OnInit } from 'angular2/core';
+
+@Component({
+    selector: 'core-app',
+    template: '<h3>Welcome to .NET Core 1.0 + MVC6 + Angular 2</h3>'
+})
+export class AppComponent { }  
