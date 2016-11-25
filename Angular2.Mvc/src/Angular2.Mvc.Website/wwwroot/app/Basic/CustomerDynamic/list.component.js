e848bb7f6e0226cfbd3d0a49165f2c8d2609d741
KarateJB <ppippenkimo@gmail.con>
2016-11-25 17:46:18 +0800

    Add the index page for dynamic loading.

--- Angular2.Mvc/src/Angular2.Mvc.Website/wwwroot/app/Basic/CustomerDynamic/list.component.js
+++ Angular2.Mvc/src/Angular2.Mvc.Website/wwwroot/app/Basic/CustomerDynamic/list.component.js
@@ -1,4 +1,4 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(['@angular/core', '../../service/customer.service', '../../service/resturi.service'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,28 +10,40 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, customer_service_1, resturi_service_1;
     var CustomerDynamicListComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (customer_service_1_1) {
+                customer_service_1 = customer_service_1_1;
+            },
+            function (resturi_service_1_1) {
+                resturi_service_1 = resturi_service_1_1;
             }],
         execute: function() {
             CustomerDynamicListComponent = (function () {
-                function CustomerDynamicListComponent() {
+                function CustomerDynamicListComponent(custService) {
+                    this.custService = custService;
                 }
                 CustomerDynamicListComponent.prototype.ngOnInit = function () {
+                    this.initCustomers();
                 };
-                CustomerDynamicListComponent.prototype.showMsg = function () {
-                    alert("Test!");
+                CustomerDynamicListComponent.prototype.initCustomers = function () {
+                    var _this = this;
+                    this.custService.getAll().then(function (data) {
+                        _this.customers = data;
+                    });
                 };
                 CustomerDynamicListComponent = __decorate([
                     core_1.Component({
                         selector: 'customerdynamic-list',
+                        providers: [customer_service_1.CustomerService, resturi_service_1.RestUriService],
                         templateUrl: '/app/Basic/CustomerDynamic/list.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [customer_service_1.CustomerService])
                 ], CustomerDynamicListComponent);
                 return CustomerDynamicListComponent;
             }());
