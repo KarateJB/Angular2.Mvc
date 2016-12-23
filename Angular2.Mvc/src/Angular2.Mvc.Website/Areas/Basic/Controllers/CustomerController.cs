8a364ebb8a453a82ee0538c793ada5fb3cc6162a
KarateJB <ppippenkimo@gmail.con>
2016-12-23 10:56:07 +0800

    Update JSNLog and NLog version and test OK.

--- Angular2.Mvc/src/Angular2.Mvc.Website/Areas/Basic/Controllers/CustomerController.cs
+++ Angular2.Mvc/src/Angular2.Mvc.Website/Areas/Basic/Controllers/CustomerController.cs
@@ -27,7 +27,11 @@ namespace Angular2.Mvc.Website.Areas.Basic.Controllers
             //Show how to log with NLog
             //base._logger.Debug($"Areas:Basic,Controller:Customer,Action:Index");
             //base._logger.Error($"Areas:Basic,Controller:Customer,Action:Index");
-            
+
+#if NET452
+            base._logger.Info("This log is for .NET framework 4.5.2 only!");
+#endif
+
             return View();
         }
 
--- Angular2.Mvc/src/Angular2.Mvc.Website/Controllers/HomeController.cs
+++ Angular2.Mvc/src/Angular2.Mvc.Website/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using NLog;
 
 namespace Angular2.Mvc.Website.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
 
         public IActionResult Index()
