99d625a1d6f3d2e1035ae399711e553e29314f46
KarateJB <ppippenkimo@gmail.con>
2017-01-06 10:41:23 +0800

    Create a MsTest with NSubstitute.

---
+++ Angular2.Mvc/test/Angular2.Mvc.MsTest/Service/SplitConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Angular2.Mvc.MsTest.Service
+{
+    public class SplitConfig : ISplitConfig
+    {
+        public char[] Separator { get; set; }
+
+        public SplitConfig()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var separator = configuration["Separator"];
+            this.Separator = separator.ToCharArray();
+        }
+    }
+}
--- Angular2.Mvc/test/Angular2.Mvc.MsTest/UnitTestDemo.cs
+++ Angular2.Mvc/test/Angular2.Mvc.MsTest/UnitTestDemo.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Angular2.Mvc.MsTest.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
 
 namespace Angular2.Mvc.MsTest
 {
@@ -15,6 +18,42 @@ namespace Angular2.Mvc.MsTest
             var input = "Luke Skywalker, Leia Skywalker, Anakin Skywalker";
             var expected = 3;
             var actual = input.Split(',').Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSplitCounter()
+        {
+            var splitConfig = new SplitConfig();
+            Debug.WriteLine(splitConfig.Separator);
+
+            var input = "Luke Skywalker, Leia Skywalker, Anakin Skywalker";
+            var expected = 3;
+            var actual = (new SplitCounter()).Calculate(input);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSplitCounterWithNSub()
+        {
+            #region Create the Substitutes
+            //Create the Nsubs for the interfaces
+            var splitConfig = Substitute.For<ISplitConfig>();
+            #endregion
+
+            #region Set the return value of the mock objects
+            splitConfig.Separator.Returns(new char[] { ';'});
+            #endregion
+
+            #region Intialize the test target
+            var splitCounter = new SplitCounter();
+            splitCounter._splitConfig = splitConfig;
+            #endregion
+
+
+            var input = "Luke Skywalker;Leia Skywalker;AnakinSkywalker";
+            var expected = 3;
+            var actual = splitCounter.Calculate(input);
             Assert.AreEqual(expected, actual);
         }
 
