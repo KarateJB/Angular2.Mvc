a393a8b4945800efa9a7c2142f613d3083b585b8
KarateJB <ppippenkimo@gmail.con>
2017-01-06 09:22:00 +0800

    Complete the MsTest sample.

---
+++ Angular2.Mvc/test/Angular2.Mvc.MsTest/UnitTestDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Angular2.Mvc.MsTest
+{
+    [TestClass]
+    public class UnitTestDemo
+    {
+        [TestMethod]
+        public void TestSplitCount()
+        {
+            var input = "Luke Skywalker, Leia Skywalker, Anakin Skywalker";
+            var expected = 3;
+            var actual = input.Split(',').Count();
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
