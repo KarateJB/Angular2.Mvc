6a61f4e3ac2a663cfa7b64e4a6db5947001d3887
KarateJB <ppippenkimo@gmail.con>
2017-01-06 08:44:55 +0800

    Create some xUnit test cases.

---
+++ Angular2.Mvc/test/Angular2.Mvc.UnitTest/UnitTestDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Angular2.Mvc.Service.Factory;
+using Xunit;
+using Angular2.Mvc.UnitTest.Model;
+
+namespace Angular2.Mvc.UnitTest
+{
+    public class UnitTestDemo
+    {
+
+        [Fact]
+        public void TestSplitCount()
+        {
+            var input = "Luke Skywalker, Leia Skywalker, Anakin Skywalker";
+            var actual = input.Split(',').Count();
+
+            Assert.True(actual.Equals(3), $"Expected:3, Actual:{actual}");
+        }
+
+        //[Theory]
+        //[InlineData("A,B,C")]
+        //[InlineData("1,2,3")]
+        //[InlineData("#,^,*")]
+        //public void TestSplitCountComplexInput(string input)
+        //{
+        //    var actual = input.Split(',').Count();
+
+        //    Assert.True(actual.Equals(3), $"Expected:3, Actual:{actual}");
+        //}
+
+        [Theory]
+        [MemberData("TestCaseIndex", MemberType = typeof(TestCase))]
+        public void TestSplitCountComplexInput(int index)
+        {
+            var input = TestCase.Data[index];
+            var value = (string)input[0];
+            var expected = (int)input[1];
+            var actual = value.Split(',').Count();
+
+            Assert.True(actual.Equals(expected), $"Expected:{expected}, Actual:{actual}");
+
+        }
+    }
+
+}
